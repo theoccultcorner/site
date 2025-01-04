31dc1f8127ab20cef2b37913a9ecb795f72c5dba
Raven The wolf <theoccultcorner@gmail.com>
2025-01-04 00:32:27 -0800

    semhome

--- src/pages/Seminary.js
+++ src/pages/Seminary.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
+import Formation from './Formation';
+import Programs from './Programs';
+import Admissions from './Admissions';
+import Degrees from './Degrees';
+import Faculty from './Faculty';
+import Events from './Events';
+import Resources from './Resources';
+import Contact from './Contact';
+import FAQs from './FAQs';
+import Apply from './Apply';
+import SeminaryHome from './SeminaryHome';
 
 const Seminary = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -14,7 +25,7 @@ const Seminary = () => {
   };
 
   const links = [
-    { label: 'Home', path: '/' },
+    { label: 'Home', path: '/SeminaryHome' },
     { label: 'Formation', path: '/formation' },
     { label: 'Programs', path: '/programs' },
     { label: 'Admissions', path: '/admissions' },
@@ -28,40 +39,56 @@ const Seminary = () => {
   ];
 
   return (
-    <AppBar position="static" style={{ backgroundColor: 'black' }}>
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
-            Seminary
-          </Link>
-        </Typography>
-        <div>
-          <IconButton
-            edge="end"
-            color="inherit"
-            onClick={handleMenuOpen}
-          >
-            <Typography>Menu</Typography>
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            {links.map((link) => (
-              <MenuItem
-                key={link.label}
-                onClick={handleMenuClose}
-                component={Link}
-                to={link.path}
-              >
-                {link.label}
-              </MenuItem>
-            ))}
-          </Menu>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static" style={{ backgroundColor: 'black' }}>
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            <Link to="/SeminaryHome" style={{ color: 'white', textDecoration: 'none' }}>
+              Seminary
+            </Link>
+          </Typography>
+          <div>
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleMenuOpen}
+            >
+              <Typography>Menu</Typography>
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              {links.map((link) => (
+                <MenuItem
+                  key={link.label}
+                  onClick={handleMenuClose}
+                  component={Link}
+                  to={link.path}
+                >
+                  {link.label}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/SeminaryHome" element={<SeminaryHome />} />
+        <Route path="/formation" element={<Formation />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/admissions" element={<Admissions />} />
+        <Route path="/degrees" element={<Degrees />} />
+        <Route path="/faculty" element={<Faculty />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/faqs" element={<FAQs />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </div>
   );
 };
 
