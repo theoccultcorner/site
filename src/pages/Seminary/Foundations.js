c0359cf14bb51bcce9ffb5b45f4f5120e96d8a49
Raven The wolf <theoccultcorner@gmail.com>
2025-01-09 23:39:56 -0800

    dhdhd

--- src/pages/Seminary/Foundations.js
+++ src/pages/Seminary/Foundations.js
@@ -4,52 +4,50 @@ import { Button } from '@mui/material';
 const Foundations = () => {
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Foundations in Gnostic Catholic Theology</h1>
+      <h1 style={styles.title}>Foundations in Gnostic Christian Theology</h1>
       <p style={styles.text}>
-        This foundational course provides a comprehensive introduction to Gnostic Catholic theology's core beliefs,
-        practices, and history. Students will delve into the ancient roots of Gnosticism and explore its evolution
-        into modern Gnostic Catholic thought. The course aims to establish a solid theological framework for further
-        studies in the Bachelor of Theology program offered by the Gnostic Catholic Union Seminary.
+        The courses provided by The Gnostic Union offer a comprehensive introduction to Gnostic Christian theology, core beliefs, practices, and history. Students will delve into the roots of ancient Christianity and explore its evolution into modern Gnostic Christian thought. The course aims to establish a solid theological framework for further studies and independent work students may decide to undertake with support from our team.
       </p>
 
       <h3 style={styles.heading}>Course Overview</h3>
       <p style={styles.text}>
-        Throughout the course, students will engage with essential texts illuminating Gnostic Catholic theology's
-        central tenets. By examining works such as:
+        Throughout the courses, students will engage with essential ancient Gnostic texts, as well as explore Gnostic theology and beliefs within canonical texts. By collaborating with and learning from our Bishops and Priests, students will examine works such as:
       </p>
       <ul style={styles.list}>
-        <li><i>Gnosticism: New Light on the Ancient Tradition of Inner Knowing</i> by Stephan A. Hoeller</li>
-        <li><i>Forbidden Faith: The Secret History of Gnosticism</i> by Richard Smoley</li>
-        <li><i>A Gnostic Book of Hours</i> by June Singer</li>
+        <li><i>The Gospel of Thomas</i></li>
+        <li><i>The Gospel of Mary</i></li>
+        <li><i>The Gospel of Philip</i></li>
+        <li><i>The Gospel of Judas</i></li>
+        <li>Bishop Nathan Wilson’s translation of the Greek Gospels:
+          <ul style={{ paddingLeft: '20px' }}>
+            <li>Matthew</li>
+            <li>Mark</li>
+            <li>John</li>
+          </ul>
+        </li>
       </ul>
       <p style={styles.text}>
         Students will gain a deeper understanding of the Gnostic approach to spirituality, self-knowledge, and the divine.
       </p>
 
-      <h3 style={styles.heading}>Key Topics Covered</h3>
-      <ul style={styles.list}>
-        <li>The historical context and development of Gnosticism</li>
-        <li>The relationship between Gnosticism and early Christianity</li>
-        <li>Central Gnostic myths, symbols, and archetypes</li>
-        <li>The role of personal experience and inner knowing in Gnostic spirituality</li>
-        <li>Gnostic views on the nature of the divine, creation, and the human condition</li>
-        <li>The integration of Gnostic principles into contemporary Gnostic Catholic theology and practice</li>
-      </ul>
-
       <h3 style={styles.heading}>Course Requirements</h3>
       <p style={styles.text}>
-        Students must write a 5-8 page synopsis for the two designated texts, demonstrating their understanding and
-        critical engagement with the material. Throughout the course, students will have the support and guidance of
-        a dedicated academic advisor who will assist them in their studies and foster their intellectual and spiritual
-        growth.
+        Students must provide a written 6-9 page synopsis for the designated texts to demonstrate their understanding and engagement with the source material. They will also submit a book review and personal reflections for texts such as:
+      </p>
+      <ul style={styles.list}>
+        <li><i>The Gospel of Thomas</i></li>
+        <li><i>The Gospel of Mary</i></li>
+        <li><i>The Gospel of Philip</i></li>
+        <li><i>The Gospel of Judas</i></li>
+        <li><i>The Proto-Gospel of James</i></li>
+      </ul>
+      <p style={styles.text}>
+        Throughout the course, students will have the support and guidance of dedicated Bishops and Priests, assisting them in their spiritual growth.
       </p>
 
       <h3 style={styles.heading}>Learning Outcomes</h3>
       <p style={styles.text}>
-        By the end of this course, students will have developed a robust foundation in Gnostic Catholic theology,
-        preparing them for advanced studies in the Bachelor of Theology program. They will have cultivated a deep
-        appreciation for the richness and complexity of Gnostic thought and its relevance to contemporary spiritual
-        practice and personal transformation.
+        By the end of this course, students will develop a deeper understanding of Gnostic Christianity. They will be prepared for advanced studies and will have cultivated a unique appreciation for the richness of Gnostic thought and its relevance to contemporary spiritual practice and personal transformation.
       </p>
 
       <h3 style={styles.heading}>Additional Information</h3>
@@ -63,12 +61,12 @@ const Foundations = () => {
         For more information, please contact:
       </p>
       <div style={styles.contact}>
-        <p>Bishop Jason Jones, G.S.J., S.T.L., M.S., DMin., Ed.D.</p>
-        <p>Rector and Academic Dean of The Gnostic Catholic Union Seminary</p>
+        <p>Bishop Nathan Wilson</p>
+        <p>Archbishop of The Gnostic Union</p>
         <Button
           variant="contained"
           color="primary"
-          href="mailto:BishopJasonJones@TheGnosticCatholicUnion.org"
+          href="mailto:BishopNathanWilson@TheGnosticUnion.org"
           style={styles.button}
         >
           Contact Us
