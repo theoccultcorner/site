1ed658ca80a2a0225226dbb947fbc89c2651c83f
Raven The wolf <theoccultcorner@gmail.com>
2025-05-14 21:13:09 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
@@ -8,60 +8,50 @@ import About from "./pages/About";
 import Seminary from "./pages/Seminary/Seminary";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-import DegreePrograms from "./pages/Seminary/DegreePrograms";
-import CertificatePrograms from "./pages/Seminary/CertificatePrograms";
-import Events from "./pages/Seminary/Events";
-import Accreditation from "./pages/Seminary/Accreditation";
-import ProfileList from "./pages/ProfileList"; // List of all profiles
-import UserProfile from "./pages/UserProfile"; // Specific user profile
-import ManageBlogs from "./pages/ManageBlogs"; // Manage blogs page
+
+import ProfileList from "./pages/ProfileList";
+import UserProfile from "./pages/UserProfile";
+import ManageBlogs from "./pages/ManageBlogs";
 import Meta from "./pages/Meta";
- 
+
 import Formation from "./pages/Seminary/Formation";
 import Requirements from "./pages/Seminary/Requirements";
 import Foundations from "./pages/Seminary/Foundations";
-
-// Newly added components
- 
+import CertificatePrograms from "./pages/Seminary/CertificatePrograms";
+import DegreePrograms from "./pages/Seminary/DegreePrograms";
 import Ministry from "./pages/Seminary/Ministry";
 import Recommended from "./pages/Seminary/Recommended";
- 
-
- 
- 
+import Accreditation from "./pages/Seminary/Accreditation";
+import Events from "./pages/Seminary/Events";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Layout for the site */}
         <Route path="/" element={<Layout />}>
-          {/* Main routes */}
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="meta" element={<Meta />} />
           <Route path="blogs" element={<Blogs />} />
-           
 
-          {/* Profile-related routes */}
-          <Route path="profilelist" element={<ProfileList />} /> {/* List of all profiles */}
-          <Route path="users/:displayName" element={<UserProfile />} /> {/* View specific user profile */}
-          <Route path="blogs/:displayName" element={<ManageBlogs />} /> {/* Manage blogs for a user */}
+          {/* Profile routes */}
+          <Route path="profilelist" element={<ProfileList />} />
+          <Route path="users/:displayName" element={<UserProfile />} />
+          <Route path="blogs/:displayName" element={<ManageBlogs />} />
 
-          {/* Seminary-related routes */}
+          {/* Seminary routes */}
           <Route path="seminary" element={<Seminary />} />
           <Route path="seminary/formation" element={<Formation />} />
           <Route path="seminary/requirements" element={<Requirements />} />
           <Route path="seminary/foundations" element={<Foundations />} />
+          <Route path="seminary/ministry" element={<Ministry />} />
+          <Route path="seminary/recommended" element={<Recommended />} />
+          <Route path="seminary/degree-programs" element={<DegreePrograms />} />
+          <Route path="seminary/certificate-programs" element={<CertificatePrograms />} />
+          <Route path="seminary/accreditation" element={<Accreditation />} />
+          <Route path="seminary/events" element={<Events />} />
 
-          {/* New seminary program routes */}
- <Route path="seminary/degree-programs" element={<DegreePrograms />} />
-<Route path="seminary/certificate-programs" element={<CertificatePrograms />} />
-<Route path="seminary/accreditation" element={<Accreditation />} />
-<Route path="seminary/events" element={<Events />} />
-
-          {/* Catch-all route */}
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
@@ -69,5 +59,4 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default App;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,64 +1,182 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import About from "./pages/About";
-import Seminary from "./pages/Seminary/Seminary";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import DegreePrograms from "./pages/Seminary/DegreePrograms";
-import CertificatePrograms from "./pages/Seminary/CertificatePrograms";
-import Events from "./pages/Seminary/Events";
-import Accreditation from "./pages/Seminary/Accreditation";
-import ProfileList from "./pages/ProfileList"; // List of all profiles
-import UserProfile from "./pages/UserProfile"; // Specific user profile
-import ManageBlogs from "./pages/ManageBlogs"; // Manage blogs page
-import Meta from "./pages/Meta";
-import Formation from "./pages/Seminary/Formation";
-import Requirements from "./pages/Seminary/Requirements";
-import Foundations from "./pages/Seminary/Foundations";
-import Ministry from "./pages/Seminary/Ministry";
-import Recommended from "./pages/Seminary/Recommended";
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Avatar,
+  Tooltip,
+  Box,
+  useMediaQuery,
+} from "@mui/material";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import Footer from "./Footer"; // Moved Footer to be in the same folder as Layout.js
+import { auth } from "../firebaseConfig"; // Adjusted to stay within src/ directory
+import {
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
-const App = () => {
+const Layout = () => {
+  const [user, setUser] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [seminaryAnchorEl, setSeminaryAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+
+      const userRef = doc(db, "profiles", result.user.uid);
+      const docSnap = await getDoc(userRef);
+      if (!docSnap.exists()) {
+        await setDoc(userRef, {
+          email: result.user.email,
+          displayName: result.user.displayName,
+          photoURL: result.user.photoURL,
+          bio: "This is a sample bio",
+          website: "https://example.com",
+        });
+      }
+
+      navigate("/profileList");
+    } catch (error) {
+      console.error("Error occurred during login:", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      setAnchorEl(null);
+    } catch (error) {
+      console.error("Error occurred during logout:", error);
+    }
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setSeminaryAnchorEl(null);
+  };
+
+  const navigateTo = (path) => {
+    handleClose();
+    navigate(path);
+  };
+
+  const handleSeminaryMenu = (event) => {
+    setSeminaryAnchorEl(event.currentTarget);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Layout for the site */}
-        <Route path="/" element={<Layout />}>
-          {/* Main routes */}
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="meta" element={<Meta />} />
-          <Route path="blogs" element={<Blogs />} />
+    <>
+      <AppBar position="static" style={{ background: "black" }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            style={{ textDecoration: "none", color: "inherit", flexGrow: 1 }}
+          >
+            The Gnostic Union
+          </Typography>
 
-          {/* Profile-related routes */}
-          <Route path="profilelist" element={<ProfileList />} />
-          <Route path="users/:displayName" element={<UserProfile />} />
-          <Route path="blogs/:displayName" element={<ManageBlogs />} />
+          {user ? (
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              {isMobile && (
+                <Typography variant="body2" color="inherit">
+                  Menu
+                </Typography>
+              )}
+              <Tooltip title="Click to access the menu" placement="bottom">
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                  style={{ position: "relative" }}
+                >
+                  {user.photoURL ? (
+                    <Avatar src={user.photoURL} alt={user.displayName} />
+                  ) : (
+                    <Avatar>
+                      {user.displayName ? user.displayName.charAt(0) : ""}
+                    </Avatar>
+                  )}
+                </IconButton>
+              </Tooltip>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                keepMounted
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => navigateTo("/about")}>About</MenuItem>
+                <MenuItem onClick={() => navigateTo("/profileList")}>Community</MenuItem>
+                <MenuItem onClick={() => navigateTo("/meta")}>Διαλεκτικὸς Χῶρος</MenuItem>
+                <MenuItem onClick={() => navigateTo("/blogs")}>Blogs</MenuItem>
+                <MenuItem onClick={handleSeminaryMenu}>Seminary ▸</MenuItem>
+                <Menu
+                  anchorEl={seminaryAnchorEl}
+                  open={Boolean(seminaryAnchorEl)}
+                  onClose={handleClose}
+                  anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                  transformOrigin={{ vertical: "top", horizontal: "left" }}
+                >
+                  <MenuItem onClick={() => navigateTo("/seminary")}>Overview</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/requirements")}>Requirements</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/recommended")}>Recommended Reading</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/ministry")}>Ministry</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/foundations")}>Foundations</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/degree-programs")}>Degree Programs</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/certificate-programs")}>Certificate Programs</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/accreditation")}>Accreditation</MenuItem>
+                  <MenuItem onClick={() => navigateTo("/seminary/events")}>Events</MenuItem>
+                </Menu>
+                <MenuItem onClick={() => navigateTo("/contact")}>Contact</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </Box>
+          ) : (
+            <Button color="inherit" onClick={handleLogin}>
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
 
-          {/* Seminary-related routes */}
-          <Route path="seminary" element={<Seminary />} />
-          <Route path="seminary/formation" element={<Formation />} />
-          <Route path="seminary/requirements" element={<Requirements />} />
-          <Route path="seminary/foundations" element={<Foundations />} />
-          <Route path="seminary/ministry" element={<Ministry />} />
-          <Route path="seminary/recommended" element={<Recommended />} />
-          <Route path="seminary/degree-programs" element={<DegreePrograms />} />
-          <Route path="seminary/certificate-programs" element={<CertificatePrograms />} />
-          <Route path="seminary/accreditation" element={<Accreditation />} />
-          <Route path="seminary/events" element={<Events />} />
-
-          {/* Catch-all route */}
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default Layout;
